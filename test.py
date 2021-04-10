fdbed8f6f612b7aea906b3c2ff639928cd6209ad
YJH16120 <yapjiahong2003@gmail.com>
2021-04-10 10:41:44 +0800

    Currently doing test with environment variables.

--- test.py
+++ test.py
@@ -1,8 +1,5 @@
 import os
-from boto.s3.connection import S3Connection
 
-try:
-    s3 = S3Connection(os.environ['API_KEY'])
-    print("Key available")
-except:
-    print("Key unavailable")
+
+res = True if "API_KEY" in os.environ else False
+print(res)
